04953ace7ecfe8668cbb290948461058814e072e
marcomessa <marco.messa3@gmail.com>
2020-03-23 19:33:08 +0100

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,24 @@
+<?php namespace MarcoMessa\ChuckNorrisJokes;
+
+class JokeFactory
+{
+    protected $jokes = [
+        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+        'Chuck Norris is the reason why Waldo is hiding.',
+        'Chuck Norris can set ants on fire with a magnifying glass. At night.'
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+
+
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MarcoMessa\ChuckNorrisJokes\Tests;
+
+use MarcoMessa\ChuckNorrisJokes\JokeFactory;
+use PHPUnit\Framework\TestCase;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
+            'Chuck Norris is the reason why Waldo is hiding.',
+            'Chuck Norris can set ants on fire with a magnifying glass. At night.'
+        ];
+
+        $jokes = new JokeFactory();
+
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
